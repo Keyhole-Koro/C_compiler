2cfd117be6e359312f2e1ad91875e877ba699126
Keyhole-Koro <130579219+Keyhole-Koro@users.noreply.github.com>
2023-07-30 17:54:40 +0900

    Update kcc.c

--- kcc.c
+++ kcc.c
@@ -5,23 +5,28 @@
 #include <stdlib.h>
 #include <string.h>
 
+// トークンの種類
 typedef enum {
-	TK_OPERATOR,
-	TK_NUM,
-	TK_EOF,
+  TK_RESERVED, // 記号
+  TK_NUM,      // 整数トークン
+  TK_EOF,      // 入力の終わりを表すトークン
 } TokenKind;
 
 typedef struct Token Token;
 
-struct Token{
-	TokenKind kind;
-	Token *next;
-	int val;
-	char *str;
+// トークン型
+struct Token {
+  TokenKind kind; // トークンの型
+  Token *next;    // 次の入力トークン
+  int val;        // kindがTK_NUMの場合、その数値
+  char *str;      // トークン文字列
 };
 
+// 現在着目しているトークン
 Token *token;
 
+// エラーを報告するための関数
+// printfと同じ引数を取る
 void error(char *fmt, ...) {
   va_list ap;
   va_start(ap, fmt);
@@ -30,23 +35,28 @@ void error(char *fmt, ...) {
   exit(1);
 }
 
+// 次のトークンが期待している記号のときには、トークンを1つ読み進めて
+// 真を返す。それ以外の場合には偽を返す。
 bool consume(char op) {
-	if (token->kind != TK_OPERATOR || token->str[0] != op) {
-		return false;
-	}
-	token=token->next;
-	return true;
+  if (token->kind != TK_RESERVED || token->str[0] != op)
+    return false;
+  token = token->next;
+  return true;
 }
 
+// 次のトークンが期待している記号のときには、トークンを1つ読み進める。
+// それ以外の場合にはエラーを報告する。
 void expect(char op) {
-  if (token->kind != TK_OPERATOR || token->str[0] != op)
-    error("this is not '%c'", op);
+  if (token->kind != TK_RESERVED || token->str[0] != op)
+    error("'%c'ではありません", op);
   token = token->next;
 }
 
+// 次のトークンが数値の場合、トークンを1つ読み進めてその数値を返す。
+// それ以外の場合にはエラーを報告する。
 int expect_number() {
   if (token->kind != TK_NUM)
-    error("this is not number");
+    error("数ではありません");
   int val = token->val;
   token = token->next;
   return val;
@@ -56,6 +66,7 @@ bool at_eof() {
   return token->kind == TK_EOF;
 }
 
+// 新しいトークンを作成してcurに繋げる
 Token *new_token(TokenKind kind, Token *cur, char *str) {
   Token *tok = calloc(1, sizeof(Token));
   tok->kind = kind;
@@ -64,53 +75,66 @@ Token *new_token(TokenKind kind, Token *cur, char *str) {
   return tok;
 }
 
+// 入力文字列pをトークナイズしてそれを返す
 Token *tokenize(char *p) {
-	Token head;
-	head.next = NULL;
-	Token *cur = &head;
-	
-	while(*p) {
-		if (isspace(*p)) {
-			p++;
-			continue;
-		}
-		if (*p == '+' || *p == '-') {
-			new_token(TK_OPERATOR, cur, p++);
-			continue;	
-		}
-		if (isdigit(*p)){
-			new_token(TK_NUM, cur, p);
-			cur->val = strtol(p, &p, 10);
-			continue;
-		}
+  Token head;
+  head.next = NULL;
+  Token *cur = &head;
 
-	error("disable to tokenize");
-	}
-	new_token(TK_EOF, cur, p++);
-	return head.next;	
+  while (*p) {
+    // 空白文字をスキップ
+    if (isspace(*p)) {
+      p++;
+      continue;
+    }
+
+    if (*p == '+' || *p == '-') {
+      cur = new_token(TK_RESERVED, cur, p++);
+      continue;
+    }
+
+    if (isdigit(*p)) {
+      cur = new_token(TK_NUM, cur, p);
+      cur->val = strtol(p, &p, 10);
+      continue;
+    }
+
+    error("トークナイズできません");
+  }
+
+  new_token(TK_EOF, cur, p);
+  return head.next;
 }
 
 int main(int argc, char **argv) {
   if (argc != 2) {
-    fprintf(stderr, "invalid arguments\n");
+    error("引数の個数が正しくありません");
     return 1;
   }
 
+  // トークナイズする
   token = tokenize(argv[1]);
-  printf(".intel_syntax noprefix");
-  printf(".extern printf\n");
-  printf(".global main\n");
+
+  // アセンブリの前半部分を出力
+  printf(".intel_syntax noprefix\n");
+  printf(".globl main\n");
   printf("main:\n");
+
+  // 式の最初は数でなければならないので、それをチェックして
+  // 最初のmov命令を出力
   printf("  mov rax, %d\n", expect_number());
 
+  // `+ <数>`あるいは`- <数>`というトークンの並びを消費しつつ
+  // アセンブリを出力
   while (!at_eof()) {
     if (consume('+')) {
       printf("  add rax, %d\n", expect_number());
       continue;
     }
+
     expect('-');
-    printf(" sub rax, %d\n", expect_number());
-    }
+    printf("  sub rax, %d\n", expect_number());
+  }
 
   printf("  ret\n");
   return 0;
