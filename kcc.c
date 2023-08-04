cec8d21a11bb6cf75e8eaa0c66684d1098357d82
Keyhole-Koro <korokororinthird@gmail.com>
2023-08-04 14:47:51 +0000

    temporary save

--- kcc.c
+++ kcc.c
@@ -6,24 +6,27 @@
 #include <string.h>
 
 typedef enum {
-  TK_OPERATOR,
-  TK_NUM,
-  TK_END,
-} TokenKind;
+  ND_EXPR,
+  ND_OPERATOR,
+  ND_NUM,
+  ND_END,
+} NodeKind;
 
-typedef struct Token Token;
+typedef struct Node Node;
 
-struct Token {
-  TokenKind kind;
-  Token *next;
+struct Node {
+  NodeKind kind;
+  Node *ltp;
+  Node *next;
+  Node *rtp;
   int value;
   char *symbol; // str
 };
 
-Token *token;
+Node *node;
 
 bool at_end() {
-  return token->kind == TK_END;
+  return node->kind == ND_END;
 }
 
 void error(char *fmt, ...) {
@@ -35,39 +38,39 @@ void error(char *fmt, ...) {
 }
 
 void expect(char op) {
-  if (token->kind != TK_OPERATOR || token->symbol[0] != op)
+  if (node->kind != ND_OPERATOR || node->symbol[0] != op)
     error("this is not '%c'", op);
-  token = token->next;
+  node = node->next;
 }
 
 int expect_number() {
-  if (token->kind != TK_NUM) {
+  if (node->kind != ND_NUM) {
     error("this is not number"); // replace to error function
   }
-  int number = token->value;
-  token = token->next;
+  int number = node->value;
+  node = node->next;
   return number;
 }
 
 bool consume(char op) {
-  if (token->kind != TK_OPERATOR || token->symbol[0] != op)
+  if (node->kind != ND_OPERATOR || node->symbol[0] != op)
     return false;
-  token = token->next;
+  node = node->next;
   return true;
 }
 
-Token *new_token(TokenKind kind, Token *current, char *input) {
-  Token *tok = calloc(1, sizeof(Token));
+Node *new_node(NodeKind kind, Node *current, char *input) {
+  Node *tok = calloc(1, sizeof(Node));
   tok->kind = kind;
   tok->symbol = input;
   current->next = tok;
   return tok;
 }
 
-Token *tokenize(char *input) {
-  Token head;
+Node *nodeize(char *input) {
+  Node head;
   head.next = NULL;
-  Token *current = &head; // updated everytime
+  Node *current = &head; // updated everytime
 
   while (*input) {
     if (isspace(*input)) {
@@ -75,17 +78,17 @@ Token *tokenize(char *input) {
       continue;
     }
     if (*input == '+' || *input == '-') {
-      current = new_token(TK_OPERATOR, current, input++);
+      current = new_node(ND_OPERATOR, current, input++);
       continue;
     }
     if (isdigit(*input)) {
-      current = new_token(TK_NUM, current, input);
+      current = new_node(ND_NUM, current, input);
       current->value = strtol(input, &input, 10); // this part increments input
       continue;
     }
-    error("disable to tokenize");
+    error("disable to nodeize");
   }
-  new_token(TK_END, current, input);
+  new_node(ND_END, current, input);
   return head.next;
 }
 
@@ -95,7 +98,7 @@ int main(int argc, char **argv) {
     return 1;
   }
 
-  token = tokenize(argv[1]);
+  node = nodeize(argv[1]);
 
   printf(".intel_syntax noprefix\n");
   printf(".extern printf\n");
