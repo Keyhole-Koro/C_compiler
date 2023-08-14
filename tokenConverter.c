e4e99cfe435b4e0f76bba13939f7c173419cf99f
Keyhole-Koro <korokororinthird@gmail.com>
2023-08-14 19:44:07 +0900

    e

--- tokenConverter.c
+++ tokenConverter.c
@@ -1,58 +1,77 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
+
+typedef struct Node Node;
 
 struct Node {
-    int data;
-    struct Node *left;
-    struct Node *right;
+    int value;
+    char *symbol;
+    Node *left;
+    Node *below;
+    Node *right;
 };
 
 int asciiToNumber(char character) {
     return (int)character;
 }
 
-struct Node *createNode(int value) {
-    struct Node *newNode = (struct Node *)malloc(sizeof(struct Node));
-    newNode->data = value;
+int convertAsciiAtOnce(char *ascii) {
+    size_t length = strlen(ascii);
+    int sum_num = 0;
+    for (size_t i = 0; i < length; i++) {
+        sum_num += asciiToNumber(ascii[i]);
+    }
+    return sum_num;
+}
+
+Node *createNode(int value, char *symbol) {
+    Node *newNode = (Node *)malloc(sizeof(Node));
+    if (newNode == NULL) {
+        perror("Memory allocation failed for new node");
+        exit(1);
+    }
+    newNode->value = value;
+    newNode->symbol = symbol;
     newNode->left = NULL;
+    newNode->below = NULL;
     newNode->right = NULL;
     return newNode;
 }
 
-struct Node *insert(struct Node *root, int value) {
-    if (root == NULL) {
-        return createNode(value);
-    }
+Node *insert(Node *root, int value, char *symbol) {
+    if (root == NULL) return createNode(value, symbol);
 
-    if (value < root->data) {
-        root->left = insert(root->left, value);
-    } else if (value > root->data) {
-        root->right = insert(root->right, value);
+    if (value < root->value) {
+        root->left = insert(root->left, value, symbol);
+    } else if (value == root->value) {
+        root->below = insert(root->below, value, symbol);
+    } else {
+        root->right = insert(root->right, value, symbol);
     }
 
     return root;
 }
 
-void inorderTraversal(struct Node *root) {
+void inorderTraversal(Node *root) {
     if (root != NULL) {
         inorderTraversal(root->left);
-        printf("%d ", root->data);
+        printf("(%d, %s) ", root->value, root->symbol);
         inorderTraversal(root->right);
     }
 }
 
 int main() {
-    struct Node *root = NULL;
-    root = insert(root, 50);
-    insert(root, 30);
-    insert(root, 20);
-    insert(root, 40);
-    insert(root, 70);
-    insert(root, 60);
-    insert(root, 80);
+    char ascii[] = "abcde";
+    int sum = convertAsciiAtOnce(ascii);
+    printf("Sum of ASCII values: %d\n", sum);
 
-    printf("Inorder traversal of the BST: ");
+    Node *root = NULL;
+    root = insert(root, sum, "abcde");
+
+    printf("Inorder traversal: ");
     inorderTraversal(root);
+    printf("\n");
 
     return 0;
 }
