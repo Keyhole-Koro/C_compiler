a221e2bec27b1b522275dcd2691164a6e1f137f1
Keyhole-Koro <korokororinthird@gmail.com>
2023-08-22 21:42:31 +0900

    a

--- expressionExample.c
+++ expressionExample.c
@@ -13,21 +13,37 @@ enum {
     DIV,      // /
     L_PARENTHESES, // (
     R_PARENTHESES, // )
+
+    EXPR, // E
+    TERM, // T
+    FACTOR, // F
 };
 
-char *productions[] = {
-    "E->E$"
-    "E->E+T",
-    "E->E-T",
-    "E->T",
-    "T->T*F",
-    "T->T/F",
-    "T->F",
-    "F->(E)",
-    "F->id"
-};
+typedef struct {
+    char *left;
+    char *right;
+    int numChar;
+    int columnTable;
+} Production;
 
-int numProductions = 9;
+typedef struct {
+    int *column;
+    int symbol;
+}
+
+Production productions[] = {
+    {"E", "E$", 2, 8},
+    {"E", "E+T", 3, 8},
+    {"E", "E-T", 3, 8},
+    {"E", "T",1 , 8},
+    {"T", "T*F", 3, 9},
+    {"T", "T/F", 3, 9},
+    {"T", "F", 1, 9},
+    {"F", "(E)", 3, 10},
+    {"F", "num", 1, 10}
+    };
+
+
 
 int *symbols[] = {NUM, ADD, SUB, MUL, DIV, L_PARENTHESES, R_PARENTHESES};
 int numSymbols = 7;
@@ -35,26 +51,10 @@ int numSymbols = 7;
 int stack[100];
 int top = -1;
 
-int getTop(){
-    return top;
-}
+int result[] = {};
+int size_result = 0;
 
-void push(int state) {
-    stack[++top] = state;
-}
 
-int pop() {
-    return stack[top--];
-}
-
-int getSymbolIndex(int symbol) {
-    for (int i = 0; i < sizeof(symbols) / sizeof(symbols[0]); i++) {
-        if (symbols[i] == symbol) {
-            return i;
-        }
-    }
-    return -1;
-}
 
 char *table[][17] = {
 //    id   +  -  *  /  (  )  $  E  T  F
@@ -76,6 +76,32 @@ char *table[][17] = {
     {"","r6","r6","r6","r6","","r6","r6","","",""}
 };
 
+bool isNum(char *letter) {
+	if (*letter < '0' || *letter > '9') return true;
+	return false;
+}
+
+int getTop(){
+    return stack[top];
+}
+
+void push(int state) {
+    stack[++top] = state;
+}
+
+int pop() {
+    return stack[top--];
+}
+
+int getSymbolIndex(int symbol) {
+    for (int i = 0; i < sizeof(symbols) / sizeof(symbols[0]); i++) {
+        if (symbols[i] == symbol) {
+            return i;
+        }
+    }
+    return -1;
+}
+
 char *readAction(char *ipt){
     int row = getTop();//state
     int column = getSymbolIndex(ipt);//symbol
@@ -84,19 +110,50 @@ char *readAction(char *ipt){
     return action;
 }
 
+char *removePrefix(char *ipt) {
+    int length = strlen(ipt);
+    char *removed_char = ipt[1, length-1];//maybe it starts with 1
+    return removed_char;
+}
+
+void addResult(int *ipt) {
+    result[size_result++] = ipt;
+}
+
+int getLateResult() {
+    return result[size_result];
+}
+
+void popAtOnce(int num) {
+    for (int i = 0; i < num; i++) {
+        top--;
+    }
+}
+
 int *parse(int *ipt){
     char *action;
-    int nextState;
+    char *prefix;
+    int next_state;
+    int nlp;
+    char *non_terminal;
     push(0);
     while(*ipt) {
-        action = readAction(ipt++);
-        if (action[0] == "s") {
-            nextState = action[1];
-            push(nextState);
+        action = readAction(ipt);
+        prefix = action[0];//head of action like r s , goto is gonna be number
+        if (*prefix == "s") {
+            next_state = removePrefix(action) - '0';
+            push(next_state);
+            ipt++;
             continue;
-        } else if (action[0] == "r") {
-
-        } else if (action[0] == "a") {
+        } else if (*prefix == "r") {
+            addResult(removePrefix(action) - '0');
+            nlp = productions[getLateResult()].numChar;
+            popAtOnce(nlp);
+            action = table[getTop()][productions[getLateResult()].columnTable];
+        } else if (isNum(prefix)) {
+            next_state = removePrefix(action) - '0';
+            push(next_state);
+        }else if (*prefix == "a") {
 
         }
     }
